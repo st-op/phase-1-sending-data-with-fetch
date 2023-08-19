b69a3f3b570a11952a8dc18f5bfd4076d46c9bab
Stella Johansson <stellajoh9@gmail.com>
2023-08-19 16:19:29 +0200

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+let theInfo = {
+    name: "Steve",
+    email: "steve@steve.com"
+}
+function submitData(name, email) {
+const postRequest = {
+    method:'POST',
+    headers: {
+        "content-type": 'application/json',
+        "accept": 'application/json'
+    },
+    body: JSON.stringify(theInfo)
+};
+let fun = (
+fetch('http://localhost:3000/users', postRequest)
+.then(res => res.json())
+.then(data => document.body.append(data.id))
+.catch(function (error) {
+    alert('Unauthorized Access')
+    document.body.append(error.message)
+}))
+return fun
+}
